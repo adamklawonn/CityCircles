83e6767037249ce488f146b6ec65e09d847f04bc
Caige Nichols <caigesn@gmail.com>
2009-10-02 22:15:01 -0700

    Updated to rails 2.3.4. Added the rest of capistrano deployment recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "citycircles"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+set :repository,  "git://github.com/adamklawonn/CityCircles.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "67.23.22.72"                          # Your HTTP server, Apache/etc
+role :app, "67.23.22.72"                          # This may be the same as your `Web` server
+role :db,  "67.23.22.72", :primary => true # This is where Rails migrations will run
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :deploy_to, "/var/www-apps/#{application}"
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :user, "capy"
+set :ssh_options, { :forward_agent => true }
+
+require 'config/deploy/capistrano_database'
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end  
+end
