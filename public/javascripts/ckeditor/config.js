be8353e4e92ec2e83c369333b33ec16c3cec179c
caiges <caigesn@gmail.com>
2009-10-10 23:07:17 -0700

    Setup default ckeditor configuration.

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -5,7 +5,17 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.toolbar = 'CityCircles';
+	config.width = "450";
+	config.resize_enabled = false;
+  config.toolbar_CityCircles =
+  [
+      ['Maximize','Cut','Copy','Paste','PasteText','PasteFromWord','-','Scayt'],
+      ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+      ['Link','Unlink','Anchor', '-','Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+      '/',
+      ['Styles','Format'],
+      ['Bold','Italic','Strike'],
+      ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote']
+  ];
 };
--- public/javascripts/ckeditorManager.js
+++ public/javascripts/ckeditorManager.js
@@ -1,14 +1,14 @@
 var ckeditorManager = new function() {
 
   this.instances = [];
-  
+
   this.makeInstance = function( instance_name ) {
     
     if( this.instances[ instance_name ] == null ) {
-      this.instances[ instance_name ] = CKEDITOR.replace( instance_name, { toolbar : 'Basic' } );
+      this.instances[ instance_name ] = CKEDITOR.replace( instance_name, { toolbar : 'CityCircles' } );
     } else {
       CKEDITOR.remove( this.instances[ instance_name ] );
-      this.instances[ instance_name ] = CKEDITOR.replace( instance_name, { toolbar : 'Basic' } );
+      this.instances[ instance_name ] = CKEDITOR.replace( instance_name, { toolbar : 'CityCircles' } );
     }
     
   };
