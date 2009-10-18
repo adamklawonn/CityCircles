5b13539ed49ecaa334e5f75ae8b1081c108827f8
caiges <caigesn@gmail.com>
2009-10-18 14:20:21 -0700

    Added SSL filters to user login and profile pages.

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "geokit"
   config.gem "thoughtbot-paperclip", :lib => "paperclip", :source => "http://gems.github.com"
   config.gem "ambethia-recaptcha", :lib => "recaptcha/rails", :source => "http://gems.github.com"
-    
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
