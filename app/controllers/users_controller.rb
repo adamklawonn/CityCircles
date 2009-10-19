b8456f2c2a49ab5e21c1ef029c32e4ab5a778cc1
caiges <caigesn@gmail.com>
2009-10-18 22:03:28 -0700

    Took out SSL filters until new ip is setup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
-  ssl_required :edit, :update
+  #ssl_required :edit, :update
   
   def new
     @user = User.new
