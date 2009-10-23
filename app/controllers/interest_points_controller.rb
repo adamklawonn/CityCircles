ee9b5994af50d94785e061f020c274f37008e585
caiges <caigesn@gmail.com>
2009-10-22 22:15:42 -0700

    Removed wysiwyg editor from post content dialogs. Reset position of post content dialog on every open.

--- app/controllers/interest_points_controller.rb
+++ app/controllers/interest_points_controller.rb
@@ -22,11 +22,8 @@ class InterestPointsController < ApplicationController
       if content_types.has_key? content_type.to_sym
         render :update do | page |
           page.replace_html "postcontentform", :partial => "#{ content_type.pluralize }/#{ content_type }", :locals => { content_type.to_sym => eval( "content_types[ content_type.to_sym ].new" ), :poi => @poi }
-          page << "$j( '#postcontent' ).dialog( 'option', 'position', [ 'center', 'center' ] );$j( '#postcontent' ).dialog( 'open' );"
+          page << "$j( '#postcontent' ).dialog( 'open' );$j( '#postcontent' ).dialog( 'option', 'position', [ 'center', 'center' ] );"
           page << "$j( '#ui-dialog-title-postcontent' ).html( 'Post #{ content_type.camelize }' );"
-          page << "CCTinyMCEManager.makeInstance( '#{ content_type }_body' );"
-          page << "$j( '##{ content_type }_submit' ).click( function() { $( '#{ content_type }_body' ).innerHTML =  tinyMCE.activeEditor.getContent(); } );"
-          page << "$j( '#postcontent' ).bind( 'dialogbeforeclose', function( event, ui ) { CCTinyMCEManager.destroyInstance( '#{ content_type }_body' ); } );"
           page << "if( poiBounds == null ) {"
           page << "poiBounds = GCircle( postcontentmap, new GLatLng( #{ @poi.lat }, #{ @poi.lng } ), new GLatLng( #{ @poi.lat } + 0.004166666666667, #{ @poi.lng } ), '#000000', '#79AB75' );"
           page << "latlngMarker = null;"
