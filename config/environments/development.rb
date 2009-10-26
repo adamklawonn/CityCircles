c762f26839d82ca7c49fb95e47543d709379b36c
caiges <caigesn@gmail.com>
2009-10-26 01:17:14 -0700

    Updated registration recaptcha keys.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,3 +18,6 @@ config.action_mailer.raise_delivery_errors = false
 
 # Footnotes
 config.gem "josevalim-rails-footnotes", :lib => "rails-footnotes", :source => "http://gems.github.com"
+
+ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcZfQgAAAAAAGOII0ryh_w5BOjq7GK4Td7Y2Y3R'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcZfQgAAAAAACIo0Q5yt79bY7o6XNFtpMzz4WLQ'
