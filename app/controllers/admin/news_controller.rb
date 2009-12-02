0ae7b9caf49475d119a60c5b6efdf54d8de6c106
caiges <caigesn@gmail.com>
2009-12-02 02:17:38 -0700

    Added admin controller for posts.

--- app/controllers/admin/news_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::NewsController < Admin::AdminController
-  
-  active_scaffold :news do | config |
-    config.list.columns = [ :headline, :author, :created_at ]
-    config.create.columns = [ :headline, :body, :lat, :lng, :author ]
-    config.update.columns = [ :headline, :body, :lat, :lng, :author ]
-  end
-  
-end
---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PostsController < Admin::AdminController
+
+  active_scaffold :post
+
+end
