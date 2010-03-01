2dc30cbf68ed09b3f5e0bf3b107e57042be962df
caiges <caigesn@gmail.com>
2010-02-28 18:34:46 -0700

    Added new deploy rules for thinking sphinx.

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,5 +35,30 @@ namespace :deploy do
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
-  end  
+  end
+  
+  desc "Re-establish symlinks"
+    task :after_symlink do
+      run <<-CMD
+        rm -fr #{release_path}/db/sphinx &&
+        ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx
+      CMD
+    end
+
+    desc "Stop the sphinx server"
+    task :stop_sphinx , :roles => :app do
+      run "cd #{current_path} && rake thinking_sphinx:stop RAILS_ENV=production"
+    end
+
+    desc "Start the sphinx server" 
+    task :start_sphinx, :roles => :app do
+      run "cd #{current_path} && rake thinking_sphinx:configure RAILS_ENV=production && rake thinking_sphinx:start RAILS_ENV=production"
+    end
+
+    desc "Restart the sphinx server"
+    task :restart_sphinx, :roles => :app do
+      stop_sphinx
+      start_sphinx
+    end
+    
 end
