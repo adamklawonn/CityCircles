649aa98d22ef1568c9706d97cd3f9596347c4b0c
Integrum User <integrum@RAWR.local>
2010-03-23 15:17:17 -0700

    worked on setting up data for tests

--- features/browse_content.feature
+++ features/browse_content.feature
@@ -1,12 +1,19 @@
 Feature: Browse content
-
+@wip
   Scenario: As a guest ISBAT view a post
-    Given I have a map with the following attributes:
-    | shortname | title    | description | lat       | lng         | zoom | author_id |
-    | lightrail | Stations | blah        | 33.474644 | -111.986650 | 11   | 0         |
-    Given I have a post with the following attributes:
-    | headline     | short_headline | body                  | lat     | interest_point_id | map_layer_id | author_id |
-    | Test Heading | TH             | Some sample body test | 21.2344 | 0                 | 0            | 0         |
+    Given there is an author
+    And there is a map
+    And there is a map icon
+    And there is an events map layer
+    And there is an interest point
+    And there is an event post type
+    And there is a post of type event
+    # Given I have a map with the following attributes:
+    # | shortname | title    | description | lat       | lng         | zoom | author_id |
+    # | lightrail | Stations | blah        | 33.474644 | -111.986650 | 11   | 0         |
+    # Given I have a post with the following attributes:
+    # | headline     | short_headline | body                  | lat     | interest_point_id | map_layer_id | author_id |
+    # | Test Heading | TH             | Some sample body test | 21.2344 | 0                 | 0            | 0         |
     Given I am on the home page
     Then show me the page
     Then I should see "Test Heading"
--- features/step_definitions/data_generation.rb
+++ features/step_definitions/data_generation.rb
@@ -2,7 +2,7 @@ Given /^I have a post with the following attributes:$/ do |table|
   pt = Factory.create(:post_type)
   map = Map.find_by_shortname( "lightrail")
   table.hashes.each do |attributes|
-    Post.create!(attributes.merge({:post_type_id => pt.id, :ma}))
+    Post.create!(attributes.merge({:post_type_id => pt.id}))
   end
 end
 
@@ -10,4 +10,32 @@ Given /^I have a map with the following attributes:$/ do |table|
   table.hashes.each do |attributes|
     Map.create!(attributes)
   end
+end
+
+Given /^there is an author$/ do
+  User.create( :login => 'citycircles', :email => 'caigesn@gmail.com', :password => 'dailyphx', :password_confirmation => 'dailyphx' )
+end
+
+Given /^there is a map$/ do
+  Map.create( :title => "Stations", :description => "METRO Light Rail is a 20-mile (32 km) light rail line operating in the cities of Phoenix, Tempe, and Mesa, Arizona and is part of the Valley Metro public transit system. Construction began in March 2005; operation started December 27, 2008.", :shortname => "lightrail", :lat => 33.474644, :lng => -111.98665, :zoom => 11, :author_id => User.first.id )
+end
+
+Given /^there is a map icon$/ do
+  MapIcon.create(:shortname => "default", :image_url => "http://maps.gstatic.com/intl/en_us/mapfiles/markerTransparent.png", :icon_size => "20, 20", :author_id => User.first.id)
+end
+
+Given /^there is an events map layer$/ do
+  MapLayer.create( :title => "Events", :shortname => "events", :author_id => User.first.id, :map_id => Map.first.id )
+end
+
+Given /^there is an interest point$/ do
+  InterestPoint.create( :label => 'Montebello / 19th Ave', :lat => 33.519894, :lng => -112.099709, :twitter_hashtag => '19mon', :map_layer_id => MapLayer.first.id, :author_id => User.first.id, :map_id => Map.first.id, :map_icon_id => MapIcon.first.id )
+end
+
+Given /^there is an event post type$/ do
+  PostType.create( :name => "Event", :map_layer_id => MapLayer.first.id, :map_icon_id => MapIcon.first.id, :map_fill_color => "#6677B6", :map_stroke_color => "#4C5A8B", :map_stroke_width => 2, :shortname => "events", :twitter_hashtag => "e" )
+end
+
+Given /^there is a post of type event$/ do
+  Post.create( :headline => 'Test Headline', :short_headline => 'Short Headline', :body => 'Test Body', :author_id => User.first.id, :lat => 33.519894, :lng => -112.099709, :interest_point_id => InterestPoint.first.id, :map_layer_id => MapLayer.first.id, :post_type_id => PostType.first.id )
 end
