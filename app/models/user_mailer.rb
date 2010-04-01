a4ce0a338d784f781a0a392d3df1015d0885c1d8
caiges <caigesn@gmail.com>
2010-03-31 23:52:56 -0700

    Updated user mailer with correct info.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ActionMailer::Base
   
   def registration(user)
-    recipients  "britton.halle@gmail.com"
-    from        "britton.halle@gmail.com"
+    recipients  user.email
+    from        "members@citycircles.com"
     subject     "Thank you for Registering"
     body        :user => user
   end
   
   def welcome(user)
-    recipients  "britton.halle@gmail.com"
-    from        "britton.halle@gmail.com"
+    recipients  user.email
+    from        "members@citycircles.com"
     subject     "Thank you for Registering"
     body        :user => user
   end
